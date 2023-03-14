c4000d88198f4175b73d9955c514e96d58b53f52
Destross <felipesagredogalindo@gmail.com>
2023-03-13 22:24:06 -0300

    Subiendo post

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,21 @@ import Head from "next/head";
 import NavigationBar from "./NavigationBar";
 import FooterBar from "./FooterBar";
 import Perfil from "./perfil";
+import Publicacion from "./Publicacion";
 
 export default function Home() {
   return (
     <div>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
-        <title>| | Principal | | Fs</title>
+        <title>Post FS</title>
         <link rel="icon" href="/marmota.png" />
       </Head>
 
       <NavigationBar/>
       
       <Perfil/>
-
+      <Publicacion/>
       <FooterBar/>
     </div>
   );
