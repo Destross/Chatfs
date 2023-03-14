2b04daa7d3a72e6970359ed5bcf7059a495debca
Destross <felipesagredogalindo@gmail.com>
2023-03-14 00:58:58 -0300

    revirtiendo cambios

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
         <title>Post FS</title>
         <link rel="icon" href="/marmota.png" />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2718515830886252"
-     crossorigin="anonymous"></script>
       </Head>
 
       <NavigationBar/>
