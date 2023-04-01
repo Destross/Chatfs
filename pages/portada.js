60f7e6a7ccd18ad3693c59b68835740ac132a646
Destross <felipesagredogalindo@gmail.com>
2023-04-01 02:58:31 -0300

    Diseño responsive

--- pages/portada.js
+++ pages/portada.js
@@ -3,6 +3,7 @@ import styles from "./index.module.css";
 
 const ChatGPT3Explanation = () => {
   return (
+    <div className={styles.container_explicacion_0}>
     <div className={styles.container_explicacion}>
       <h1>¡Bienvenido a Mindy-AI!</h1>
       <h2>Mindy-AI es un modelo de lenguaje natural desarrollado por FelipeSagredo.</h2>
@@ -10,6 +11,7 @@ const ChatGPT3Explanation = () => {
       <p>Este modelo ha sido entrenado en una amplia variedad de datos y puede aprender de las conversaciones que tiene con los usuarios. ¡Incluso puede realizar tareas como generar texto, traducir idiomas y mucho más!</p><br/>
       <p>Para comenzar a usar Mindy-AI, simplemente ingresa tus preguntas o solicitudes en el cuadro de texto y presiona "Enviar". Mindy-AI utiliza el motor de ChatGPT3 el cual te responderá en cuestión de segundos.</p><br/>
       <p>¡Disfruta conversando con Mindy-AI!</p>
+    </div>
     </div>
   );
 };
