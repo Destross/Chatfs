4a1671ac96ee4632870243406c2e73bd2157b551
felipesagredo <felipesagredogalindo@gmail.com>
2023-08-27 20:28:19 -0300

    Limpiando portada_1

--- pages/portada.js
+++ pages/portada.js
@@ -4,12 +4,9 @@ import styles from "./index.module.css";
 const ChatGPT3Explanation = () => {
   return (
     <div className={styles.container_explicacion_0}>
-    <div className={styles.container_explicacion}>
-      <h1>ChatFs</h1>
-      <p>En esta web puedes realizar consultas o dudas, basado en el motor de ChatGPT3.</p><br/>
-      <p>ChatGPT3 es un Modelo de Lenguaje Natural desarrollado por OpenAi.</p><br/>
-      <h2>Para comenzar a usar ChatFs, simplemente ingresa tus consultas en el cuadro de texto y presiona "Enviar".</h2><br/>
-    </div>
+      <div className={styles.container_explicacion}>
+        <h1>ChatFs</h1>
+      </div>
     </div>
   );
 };
