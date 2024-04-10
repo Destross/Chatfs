71554c51be8a2d2d77b1e3707dfffd065b56c178
felipesagredo <felipesagredogalindo@gmail.com>
2024-04-09 23:41:34 -0300

    cambiando a modelo gpt-3.5

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -28,7 +28,7 @@ export default async function (req, res) {
 
   try {
     const completion = await openai.createCompletion({
-      model: "text-davinci-003",
+      model: "gpt-3.5-turbo-instruct",
       prompt: generatePrompt(text),
       temperature: 0.1,
       max_tokens: 3500,
